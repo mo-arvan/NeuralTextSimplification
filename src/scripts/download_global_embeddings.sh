e8ffb713e3256b589491ac3b5218b6d76e5afaa8
Mo Arvan <mo.arvan@gmail.com>
2022-05-25 18:48:13 -0500

    Update scripts

--- src/scripts/download_global_embeddings.sh
+++ src/scripts/download_global_embeddings.sh
@@ -3,4 +3,6 @@ source ./base_conf.sh
 EMBED_DIR=${DIRECTORY}/embed/
 mkdir -p $EMBED_DIR
 cd $EMBED_DIR
-wget https://s3.amazonaws.com/mordecai-geo/GoogleNews-vectors-negative300.bin.gz && gunzip GoogleNews-vectors-negative300.bin.gz
+wget 'https://github.com/RaRe-Technologies/gensim-data/releases/download/word2vec-google-news-300/word2vec-google-news-300.gz'
+#cp ../GoogleNews-vectors-negative300.bin.gz . &&
+gunzip GoogleNews-vectors-negative300.bin.gz
