d5042acc7d2e86428a45a8d23eda0bd26ac7655e
marvan3 <marvan3@cs-parde-gpu-1.ececs.uic.edu>
2022-06-06 16:47:25 -0500

    update scripts

--- src/scripts/create_embeddings.sh
+++ src/scripts/create_embeddings.sh
@@ -8,11 +8,11 @@ if [[ -z "${GLOBAL_EMBED}" ]]; then
 fi 
 
 mkdir -p ${DIRECTORY}/embed
-cd ${DIRECTORY} && cat *.en > ${DIRECTORY}/embed/corpus.en && cat *.sen > ${DIRECTORY}/embed/corpus.sen && cd ${CWD}/..
+cd ${DIRECTORY} && cat train.en > ${DIRECTORY}/embed/corpus.en && cat train.sen > ${DIRECTORY}/embed/corpus.sen && cd ${CWD}/..
 python train_word2vec.py ${DIRECTORY}/embed/corpus.en 200
 python train_word2vec.py ${DIRECTORY}/embed/corpus.sen 200
 cd $OPENNMT_PATH
-th tools/concat_embedding.lua -dict_file ${DIRECTORY}/${CUR_EXP}.src.dict -global_embed ${GLOBAL_EMBED} -local_embed ${DIRECTORY}/embed/corpus.sen.bin -save_data ${DIRECTORY}/embed/sen-embeddings-${CUR_EXP}
-th tools/concat_embedding.lua -dict_file ${DIRECTORY}/${CUR_EXP}.tgt.dict -global_embed ${GLOBAL_EMBED} -local_embed ${DIRECTORY}/embed/corpus.en.bin -save_data ${DIRECTORY}/embed/en-embeddings-${CUR_EXP}
+th tools/concat_embedding.lua -dict_file ${DIRECTORY}/${CUR_EXP}.tgt.dict -global_embed ${GLOBAL_EMBED} -local_embed ${DIRECTORY}/embed/corpus.sen.bin -save_data ${DIRECTORY}/embed/sen-embeddings-${CUR_EXP}
+th tools/concat_embedding.lua -dict_file ${DIRECTORY}/${CUR_EXP}.src.dict -global_embed ${GLOBAL_EMBED} -local_embed ${DIRECTORY}/embed/corpus.en.bin -save_data ${DIRECTORY}/embed/en-embeddings-${CUR_EXP}
 
 cd $CWD
