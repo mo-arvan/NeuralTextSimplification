3239676758fb15c16722a30333959a95411e23f0
marvan3 <marvan3@cs-parde-gpu-1.ececs.uic.edu>
2022-06-28 17:24:10 -0500

    Update artifact

--- artifact/scripts/translate_and_eval_base.sh
+++ artifact/scripts/translate_and_eval_base.sh
@@ -3,15 +3,16 @@
 
 cd $OPENNMT_PATH
 
-for MODEL_VARIANT in ${MODEL_DIRECTORY}/*/; do
+for MODEL_VARIANT in ${MODEL_DIRECTORY}/*.t7; do
   MODEL_VARIANT=${MODEL_VARIANT%*/}      # remove the trailing "/"
-  for MODEL_PATH in ${MODEL_VARIANT}/*.t7; do
-    MODEL=${MODEL_PATH##*/}
-    OUTPUT=${RES_DIR}/result_${MODEL}_${BEAM_SIZE}
-    LOG_OUT=${RES_DIR}/result_${MODEL}_${BEAM_SIZE}.log
-    th translate.lua -replace_unk -beam_size ${BEAM_SIZE} -gpuid ${GPUS} -n_best 4 -model ${MODEL_PATH} -src ${SRC} -tgt ${TGT} -output ${OUTPUT} -log_file ${LOG_OUT}
-    echo $MODEL_PATH
-  done
+  MODEL_PATH=${MODEL_VARIANT}
+#  for MODEL_PATH in ${MODEL_VARIANT}/*.t7; do
+  MODEL=${MODEL_PATH##*/}
+  OUTPUT=${RES_DIR}/result_${MODEL}_${BEAM_SIZE}
+  LOG_OUT=${RES_DIR}/result_${MODEL}_${BEAM_SIZE}.log
+  th translate.lua -replace_unk -beam_size ${BEAM_SIZE} -gpuid ${GPUS} -n_best 4 -model ${MODEL_PATH} -src ${SRC} -tgt ${TGT} -output ${OUTPUT} -log_file ${LOG_OUT}
+  echo $MODEL_PATH
+ # done
 done
 
 cd $CWD
--- artifact/scripts/translate_and_eval_validation.sh
+++ artifact/scripts/translate_and_eval_validation.sh
@@ -6,13 +6,13 @@ GPUS=1
 # set the model directory
 # this directory needs to contain subdirectory for each variant
 # e.g. nts_original/nts_w2v and nts_original/nts_default
-MODEL_DIRECTORY=/workspace/nts_original
+MODEL_DIRECTORY=/workspace/models
 
 CWD=`pwd`
 DIRECTORY=`readlink -f /workspace/data`
 OPENNMT_PATH=`readlink -f /workspace/OpenNMT`
 
-RES_DIR=`readlink -f /workspace/nts_original_validation_translation`
+RES_DIR=`readlink -f /workspace/nts_w2v_validation_translation`
 mkdir -p $RES_DIR
 
 # dev_eval.en and dev_eval.sen for validation
