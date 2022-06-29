e9c59b8c8b75c5e13b9eee5a317c224690813889
Mo Arvan <mo.arvan@gmail.com>
2022-06-29 10:16:22 -0500

    Refactoring and bug fixes

--- artifact/scripts/run_sacre_bleu.sh
+++ artifact/scripts/run_sacre_bleu.sh
@@ -2,7 +2,8 @@
 
 export TEST_REFERENCE_LIST="data/references/test_references_0 data/references/test_references_1 data/references/test_references_2 data/references/test_references_3 data/references/test_references_4 data/references/test_references_5 data/references/test_references_6 data/references/test_references_7 data/references/test_references_8"
 
-export SACREBLEU_EXPORT_DIR="/workspace/artifact/results/empirical/sacrebleu_2/"
+export SACREBLEU_EXPORT_DIR="/workspace/nts_output/sacrebleu/"
+mkdir -p $SACREBLEU_EXPORT_DIR
 
 export TRANSLATION_DIR="/workspace/artifact/results/translations/o1"
 
@@ -12,21 +13,8 @@ sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -l
 export FILE_NAME="NTS_default_b5_h1"
 sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
 
-export TRANSLATION_DIR="/workspace/artifact/results/nts_w2v_test_translation"
 
-export FILE_NAME="result_nts_w2v_epoch10_10.22.t7_5_h1"
-sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
-
-export FILE_NAME="result_nts_w2v_mismatched_epoch10_10.40.t7_5_h1"
-sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
-
-export FILE_NAME="result_nts_w2v_contaminated_mismatched_epoch10_10.38.t7_5_h1"
-sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
-
-export FILE_NAME="result_nts_w2v_contaminated_epoch11_10.21.t7_5_h1"
-sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
-
-export TRANSLATION_DIR="/workspace/artifact/results/nts_original_test_translation"
+export TRANSLATION_DIR="/workspace/artifact/results/translations/o4_test"
 
 export FILE_NAME="result_NTS-w2v_epoch11_10.20_release.t7_5_h1"
 sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
@@ -34,7 +22,7 @@ sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -l
 export FILE_NAME="result_NTS_epoch11_10.19_release.t7_5_h1"
 sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
 
-export TRANSLATION_DIR="/workspace/artifact/results/nts_arvan_test_translation"
+export TRANSLATION_DIR="/workspace/artifact/results/translations/o5_test"
 
 export FILE_NAME='result_nts_word2vec_1_contaminated_mismatched_epoch10_10.48.t7_5_h1'
 sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
@@ -50,3 +38,17 @@ sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -l
 
 export FILE_NAME='result_nts_default_1_epoch14_10.49.t7_5_h1'
 sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
+
+#export TRANSLATION_DIR="/workspace/artifact/results/translations/nts_w2v_test_translation"
+#
+#export FILE_NAME="result_nts_w2v_epoch10_10.22.t7_5_h1"
+#sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
+#
+#export FILE_NAME="result_nts_w2v_mismatched_epoch10_10.40.t7_5_h1"
+#sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
+#
+#export FILE_NAME="result_nts_w2v_contaminated_mismatched_epoch10_10.38.t7_5_h1"
+#sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
+#
+#export FILE_NAME="result_nts_w2v_contaminated_epoch11_10.21.t7_5_h1"
+#sacrebleu $TEST_REFERENCE_LIST -i $TRANSLATION_DIR/$FILE_NAME -f json -m bleu -lc --force >$SACREBLEU_EXPORT_DIR/$FILE_NAME.json
